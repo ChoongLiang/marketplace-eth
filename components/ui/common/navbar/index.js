8108b1832e1a0dcef276e7383da1b23688cdf191
Thomas <choongli@buffalo.edu>
2021-12-31 02:18:21 +0800

    Disable button

--- components/ui/common/navbar/index.js
+++ components/ui/common/navbar/index.js
@@ -1,14 +1,16 @@
 import { useWeb3 } from "@components/providers";
 import Link from "next/link";
 import { Button } from "@components/ui/common";
+import { useRouter } from "next/router";
 
 export default function Navbar() {
   const { isWeb3Loaded, isLoading, connect } = useWeb3();
+  const router = useRouter();
   return (
     <section>
       <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
         <nav className="relative" aria-label="Global">
-          <div className="flex justify-between">
+          <div className="flex justify-between items-center">
             <div>
               <Link href="/">
                 <a
@@ -45,11 +47,19 @@ export default function Navbar() {
                 </a>
               </Link>
               {isLoading ? (
-                <Button onClick={connect}>Loading</Button>
+                <Button onClick={connect} disabled={true}>
+                  Loading
+                </Button>
               ) : isWeb3Loaded ? (
                 <Button onClick={connect}>Connect</Button>
               ) : (
-                <Button onClick={connect}>Install Metamask</Button>
+                <Button
+                  onClick={() => {
+                    router.push("https://metamask.io/");
+                  }}
+                >
+                  Install Metamask
+                </Button>
               )}
             </div>
           </div>
