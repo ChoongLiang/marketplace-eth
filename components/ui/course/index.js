96ca1a34ea7d01cc1b93060d951255cda640ce5f
Thomas <choongli@buffalo.edu>
2022-01-04 16:20:12 +0800

    Market pages

--- components/ui/course/index.js
+++ components/ui/course/index.js
@@ -3,3 +3,4 @@ export { default as Hero } from "./hero";
 export { default as Keypoints } from "./keypoints";
 export { default as Curriculum } from "./curriculum";
 export { default as CourseCard } from "./card";
+export { default as OwnedCourseCard } from "./card/OwnedCourseCard";
--- components/ui/marketplace/header/index.js
+++ components/ui/marketplace/header/index.js
@@ -1,13 +1,28 @@
 import { WalletBar, EthRates } from "@components/ui/web3";
 import { Breadcrumbs } from "@components/ui/common";
 
+const LINKS = [
+  {
+    href: "/marketplace",
+    value: "Marketplace",
+  },
+  {
+    href: "/marketplace/courses/owned",
+    value: "My Courses",
+  },
+  {
+    href: "/marketplace/courses/manage",
+    value: "Manage Courses",
+  },
+];
+
 export default function Header() {
   return (
     <>
       <WalletBar />
       <EthRates />
       <div className="flex flex-row-reverse">
-        <Breadcrumbs />
+        <Breadcrumbs items={LINKS} />
       </div>
     </>
   );
