0c077eec0728ccb1caa1d72bec877fbafa2f17ca
Thomas <choongli@buffalo.edu>
2022-01-05 12:13:59 +0800

    Component changes + Message

--- pages/marketplace/courses/owned.js
+++ pages/marketplace/courses/owned.js
@@ -1,12 +1,16 @@
 import { MarketHeader } from "@components/ui/marketplace";
 import { OwnedCourseCard } from "@components/ui/course";
+import { Button, Message } from "@components/ui/common";
 
 export default function ManageCourses() {
   return (
     <>
       <MarketHeader />
       <section className="grid grid-cols-1">
-        <OwnedCourseCard />
+        <OwnedCourseCard>
+          <Message>My custom message!</Message>
+          <Button>Watch the course</Button>
+        </OwnedCourseCard>
       </section>
     </>
   );
