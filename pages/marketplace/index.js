f5743cce050628c6685c2f99d20983a74aef2972
Thomas <choongli@buffalo.edu>
2022-01-05 16:46:21 +0800

    prepare order values for contract

--- pages/marketplace/index.js
+++ pages/marketplace/index.js
@@ -5,10 +5,42 @@ import { Button } from "@components/ui/common";
 import OrderModal from "@components/ui/order/modal";
 import { useState } from "react";
 import { MarketHeader } from "@components/ui/marketplace";
+import { useWeb3 } from "@components/providers";
 
 export default function Marketplace({ courses }) {
+  const { web3 } = useWeb3();
   const [selectedCourse, setSelectedCourse] = useState(null);
-  const { canPurchase } = useWalletInfo();
+  const { canPurchase, account } = useWalletInfo();
+
+  const purchaseCourse = (order) => {
+    // First course id: 1410474 => 31 34 31 30 34 37 34
+    // 0x31343130343734
+    const hexCourseId = web3.utils.utf8ToHex(selectedCourse.id);
+
+    // 0x31343130343734000000000000000000 - course
+    // 0xE9455ad2a70916544c6a65163eb3fEd38Dbc5A4A - address
+    // 31343130343734000000000000000000E9455ad2a70916544c6a65163eb3fEd38Dbc5A4A - combine
+    // get 241b6c21ddaaaff355b023143fe4e1f9cd2d6130bc8e4754855a6b0d198839d7
+    const orderHash = web3.utils.soliditySha3(
+      { type: "bytes16", value: hexCourseId },
+      { type: "address", value: account.data }
+    );
+
+    // test@gmail.com - email
+    // af257bcc3cf653863a77012256c927f26d8ab55c5bea3751063d049d0538b902
+    const emailHash = web3.utils.sha3(order.email);
+
+    // af257bcc3cf653863a77012256c927f26d8ab55c5bea3751063d049d0538b902 - email
+    // 241b6c21ddaaaff355b023143fe4e1f9cd2d6130bc8e4754855a6b0d198839d7 - order
+    // af257bcc3cf653863a77012256c927f26d8ab55c5bea3751063d049d0538b902241b6c21ddaaaff355b023143fe4e1f9cd2d6130bc8e4754855a6b0d198839d7
+    // get 0xecdb4f91629c2274ee5d518f353c917acac9badad626d94034ab46e3f4a24596
+    const proofHash = web3.utils.soliditySha3(
+      { type: "bytes32", value: emailHash },
+      { type: "bytes32", value: orderHash }
+    );
+
+    console.log(proofHash);
+  };
 
   return (
     <>
@@ -37,7 +69,7 @@ export default function Marketplace({ courses }) {
         <OrderModal
           course={selectedCourse}
           onClose={() => setSelectedCourse(null)}
-          onSubmit={(order) => alert(JSON.stringify(order))}
+          onSubmit={purchaseCourse}
         />
       )}
     </>
