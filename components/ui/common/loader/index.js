227865d4198b2e83b952413c346720b3e31f82db
Thomas <choongli@buffalo.edu>
2022-01-06 14:03:12 +0800

    course status

--- components/ui/common/loader/index.js
+++ components/ui/common/loader/index.js
@@ -6,9 +6,12 @@ const SIZES = {
 
 export default function Loader({ size = "md" }) {
   return (
-    <div className={`sk-chase ${SIZES[size]}`}>
+    <div className={`sk-chase ${SIZES[size]} `}>
       {Array.from({ length: 12 }).map((_, i) => (
-        <div key={`dot-${i}`} className={`sk-chase-dot${i + 1} sk-chase-dot`} />
+        <div
+          key={`dot-${i}`}
+          className={`sk-chase-dot${i + 1} sk-chase-dot`}
+        ></div>
       ))}
     </div>
   );
--- pages/marketplace/courses/owned.js
+++ pages/marketplace/courses/owned.js
@@ -3,23 +3,33 @@ import { OwnedCourseCard } from "@components/ui/course";
 import { Button, Loader, Message } from "@components/ui/common";
 import { getAllCourses } from "content/courses/fetcher";
 import { useAccount, useOwnedCourses } from "@components/hooks/web3";
+import { useRouter } from "next/router";
 
 export default function OwnedCourses({ courses }) {
   const { account } = useAccount();
   const { ownedCourses } = useOwnedCourses(courses, account);
+  const router = useRouter();
   return (
     <>
       <MarketHeader />
       <section className="grid grid-cols-1">
         {ownedCourses.data ? (
           ownedCourses.data.map((course) => (
-            <OwnedCourseCard course={course}>
+            <OwnedCourseCard course={course} key={course.id}>
               <Message>My custom message!</Message>
-              <Button>Watch the course</Button>
+              <Button
+                onClick={() => {
+                  router.push(`/courses/${course.slug}`);
+                }}
+              >
+                Watch the course
+              </Button>
             </OwnedCourseCard>
           ))
         ) : (
-          <Loader />
+          <div className="m-auto mt-20">
+            <Loader />
+          </div>
         )}
       </section>
     </>
